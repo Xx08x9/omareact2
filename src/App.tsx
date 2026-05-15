6ed357a5a60fba2630235ae7eebcd15622d9a23d
alentai <omorbekteam@gmail.com>
2026-05-15 17:13:39 +0600

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -4,5 +4,15 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
 
+  return (
+    <div>
+      <h1>Hello React</h1>
+
+      <button onClick={() => setCount(count + 1)}>
+        Count: {count}
+      </button>
+    </div>
+  );
+}
 
 export default App;
